3dcfc3de59171cc66632ecc78a6c8f623028cb44
buzzie-bee <39500959+buzzie-bee@users.noreply.github.com>
2021-02-03 19:00:02 +0100

    Built data entry with fetching results from firebase

--- src/features/DataEntry/DataEntry.js
+++ src/features/DataEntry/DataEntry.js
@@ -1,5 +1,96 @@
-import React, { useEffect } from 'react';
+import {
+  Button,
+  Container,
+  makeStyles,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { db } from '../../firebase/firebase';
 
 export const DataEntry = () => {
-  return <div>Data Entry</div>;
+  const [nextFewDocuments, setNextFewDocuments] = useState([]);
+  const classes = useStyles();
+
+  const fetchNextTenDocuments = async () => {
+    const documents = [];
+
+    try {
+      const locationsRef = await db.collection('locations');
+      const querySnapshot = await locationsRef
+        .where('imgUrl', '==', '')
+        .limit(10)
+        .get();
+      querySnapshot.forEach((doc) => {
+        documents.push(doc.data());
+      });
+      setNextFewDocuments(documents);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchNextTenDocuments();
+  }, []);
+
+  return (
+    <Container maxWidth="md">
+      <Typography variant="h4" className={classes.headline}>
+        Data Entry Portal
+      </Typography>
+      <div style={{ marginTop: '32px' }} />
+      <Button
+        component={Link}
+        to={
+          nextFewDocuments.length
+            ? `/entry/${nextFewDocuments[0].query}`
+            : '/entry'
+        }
+        size="large"
+        color="primary"
+        variant="contained"
+      >
+        Start Logging
+      </Button>
+      <div style={{ marginTop: '32px' }} />
+      <Typography variant="h6">Next few records:</Typography>
+      <TableContainer component={Paper} className={classes.table}>
+        <Table className={classes.table} aria-label="table of next locations">
+          <TableHead>
+            <TableCell>IATA</TableCell>
+            <TableCell>SkyCode</TableCell>
+            <TableCell>Query</TableCell>
+          </TableHead>
+          <TableBody>
+            {nextFewDocuments.map((document, idx) => {
+              return (
+                <TableRow key={`tr ${idx}`} component="th" scope="row">
+                  <TableCell>{document.iata}</TableCell>
+                  <TableCell>{document.skyCode}</TableCell>
+                  <TableCell>{document.query}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+  );
 };
+
+const useStyles = makeStyles((theme) => ({
+  headline: {
+    paddingTop: theme.spacing(2),
+  },
+  table: {
+    width: 650,
+  },
+}));

6703d0137eb67de5e91b2d5899760df0307f9eff
buzzie-bee <39500959+buzzie-bee@users.noreply.github.com>
2021-02-06 14:02:24 +0100

    Added stats to data entry page

--- src/features/DataEntry/DataEntry.js
+++ src/features/DataEntry/DataEntry.js
@@ -17,6 +17,9 @@ import { db } from '../../firebase/firebase';
 
 export const DataEntry = () => {
   const [nextFewDocuments, setNextFewDocuments] = useState([]);
+  const [statsLoading, setStatsLoading] = useState(true);
+  const [total, setTotal] = useState(0);
+  const [completed, setCompleted] = useState(0);
   const classes = useStyles();
 
   const fetchNextTenDocuments = async () => {
@@ -38,7 +41,27 @@ export const DataEntry = () => {
     }
   };
 
+  const fetchStats = async () => {
+    try {
+      const doc = await db.collection('stats').doc('stats').get();
+
+      if (doc.exists) {
+        const { total, count } = doc.data();
+        if (!total || !count) {
+          console.log('no stats found');
+          return;
+        }
+        setTotal(total);
+        setCompleted(count);
+        setStatsLoading(false);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   useEffect(() => {
+    fetchStats();
     fetchNextTenDocuments();
   }, []);
 
@@ -62,18 +85,40 @@ export const DataEntry = () => {
         Start Logging
       </Button>
       <div style={{ marginTop: '32px' }} />
+      <span>
+        <Typography variant="h6" display="inline">
+          Completed:{' '}
+        </Typography>
+        <Typography variant="h5" display="inline">
+          {statsLoading ? 'loading' : completed}
+        </Typography>
+      </span>
+      <div style={{ marginTop: '8px' }} />
+      <span>
+        <Typography variant="h6" display="inline">
+          Total:{' '}
+        </Typography>
+        <Typography variant="h5" display="inline">
+          {statsLoading ? 'loading' : total}
+        </Typography>
+      </span>
+
+      <div style={{ marginTop: '32px' }} />
       <Typography variant="h6">Next few records:</Typography>
       <TableContainer component={Paper} className={classes.table}>
         <Table className={classes.table} aria-label="table of next locations">
           <TableHead>
-            <TableCell>IATA</TableCell>
-            <TableCell>SkyCode</TableCell>
-            <TableCell>Query</TableCell>
+            <TableRow key={`thr`} component="tr" scope="row">
+              <TableCell>IATA</TableCell>
+              <TableCell>SkyCode</TableCell>
+              <TableCell>Query</TableCell>
+              <TableCell>Frequency</TableCell>
+            </TableRow>
           </TableHead>
           <TableBody>
             {nextFewDocuments.map((document, idx) => {
               return (
-                <TableRow key={`tr ${idx}`} component="th" scope="row">
+                <TableRow key={`tr ${idx}`} component="tr" scope="row">
                   <TableCell>{document.iata}</TableCell>
                   <TableCell>{document.skyCode}</TableCell>
                   <TableCell>{document.query}</TableCell>
